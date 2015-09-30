6de6706bf3c06fa88a952dfc5cb0fe22b137899a
Brian Hoffman <brianchoffman@gmail.com>
2015-09-30 15:27:32 -0400

    Start python with no console

--- windows/OpenBazaar.ps1
+++ windows/OpenBazaar.ps1
@@ -3,7 +3,7 @@ $fullDirName = "$env:localappdata\$tempDir"
 
 Set-Location "$env:localappdata\$tempDir\OpenBazaar-Client"
 
-Start-Process "python" -ArgumentList "$fullDirName\OpenBazaar-Server\openbazaard.py start" -NoNewWindow
+Start-Process "pythonw" -ArgumentList "$fullDirName\OpenBazaar-Server\openbazaard.py start" -NoNewWindow
 
 Start-Process "npm" -ArgumentList "install" -NoNewWindow -Wait
 Start-Process "npm" -ArgumentList "start"
