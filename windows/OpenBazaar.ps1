d0550691c01ad9f09b3fee3cc05d70c93a15a97c
Brian Hoffman <brianchoffman@gmail.com>
2015-09-30 15:43:34 -0400

    Start system tray icon code

--- windows/OpenBazaar.ps1
+++ windows/OpenBazaar.ps1
@@ -1,9 +1,15 @@
 $tempDir = "OpenBazaar"
 $fullDirName = "$env:localappdata\$tempDir"
 
+# Start System Tray Icon
+Set-Location "$env:localappdata\$tempDir"
+Start-Process "pythonw" -ArgumentList "systray.py" -NoNewWindow
+
 Set-Location "$env:localappdata\$tempDir\OpenBazaar-Client"
 
+# Start openbazaard
 Start-Process "pythonw" -ArgumentList "$fullDirName\OpenBazaar-Server\openbazaard.py start" -NoNewWindow
 
+# Start Electron client
 Start-Process "npm" -ArgumentList "install" -NoNewWindow -Wait
 Start-Process "npm" -ArgumentList "start"
