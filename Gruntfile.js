4340fb4d22e352cd86121a0558688c95443f05bd
Brian Hoffman <brianchoffman@gmail.com>
2016-02-29 17:28:58 -0500

    Update Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,29 +3,31 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    'electron-debian-installer': {
-
+    'electron-installer-debian': {
       options: {
         productName: 'OpenBazaar',
+        arch: 'amd64',
         rename: function (dest, src) {
           return dest + '<%= name %>_<%= version %>-<%= revision %>_<%= arch %>.deb';
+        },
+        productDescription: 'Decentralized Peer to Peer Marketplace for Bitcoin',
+        lintianOverrides: [
+          'changelog-file-missing-in-native-package',
+          'executable-not-elf-or-script',
+          'extra-license-file'
+        ],
+        icon: 'OpenBazaar-Client/imgs/openbazaar-icon.png',
+        categories: [
+          'Utility'
+        ]
+      },
+      'app-with-asar': {
+        src: 'temp-linux64/openbazaar-linux-x64/',
+        dest: 'build-linux64/',
+        rename: function (dest) {
+          return path.join(dest, '<%= name %>_<%= arch %>.deb')
         }
-      },
-      linux32: {
-        options: {
-          arch: 'i386'
-        },
-        src: 'temp/OpenBazaar-linux-ia32/',
-        dest: 'temp/'
-      },
-      linux64: {
-        options: {
-          arch: 'amd64'
-        },
-        src: 'temp/OpenBazaar-linux-x64/',
-        dest: 'temp/'
       }
-
     },
 	'create-windows-installer': {
 	  x64: {
@@ -47,10 +49,10 @@ module.exports = function(grunt) {
 	}
   });
 
-  grunt.loadNpmTasks('grunt-electron-debian-installer');
+  grunt.loadNpmTasks('grunt-electron-installer-debian');
   grunt.loadNpmTasks('grunt-electron-installer');
 
   // Default task(s).
-  grunt.registerTask('default', ['electron-debian-installer']);
+  grunt.registerTask('default', ['electron-installer-debian']);
 
 };
